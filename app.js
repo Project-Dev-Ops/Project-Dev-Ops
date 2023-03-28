d9c80dc15b803b0b81490a434b2cfddddc842eda
Baibua <siriyakorn14869@gmail.com>
2023-03-28 15:26:36 +0700

    create home page

---
+++ app.js
@@ -0,0 +1,58 @@
+// import express, body-parser, ejs and lodash to app.js
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const lodash = require("lodash");
+
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//set the bodyParser to express app
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//set the serving static files in the express in a directory named public
+app.use('/public', express.static("public"));
+
+// set get require
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+});
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about')
+});
+
+app.get('/menu', (req, res) => {
+    res.render('menu')
+});
+
+app.get('/order', (req, res) => {
+    res.render('order')
+});
+
+
+app.get('/information', (req,res) =>{
+    res.render('information')
+});
+
+app.get('/tracking', (req, res) => {
+    res.render('tracking')
+});
+
+
+app.listen('3000', function(err){
+    if(!err){
+        console.log("Server is running on port 3000.")
+    }
+})
